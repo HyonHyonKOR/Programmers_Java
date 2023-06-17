c1ddb9393aa75af9f25b82e2f831d75c213c8867
HyonHyonKOR <134394081+HyonHyonKOR@users.noreply.github.com>
2023-06-17 19:56:22 +0900

    [unrated] Title: 문자열의 앞의 n글자, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -1,6 +1,8 @@
 class Solution {
     public String solution(String my_string, int n) {
-        String answer = my_string.substring(0,n);        
+        String answer="";
+        if(my_string.length()>=1&&my_string.length()>=n&&my_string.length()<=1000)
+        {answer =my_string.substring(0,n);}         
         return answer;
     }
 }
