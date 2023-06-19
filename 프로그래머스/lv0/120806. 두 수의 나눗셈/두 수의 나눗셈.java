3b11c23eb004be17710e44b8440ef62b19f4a8d0
HyonHyonKOR <134394081+HyonHyonKOR@users.noreply.github.com>
2023-06-19 10:48:47 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,11 +1,8 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer=0;
-        if(num1>0 && num2 >0 && num1<=100 && num2<=100){
-        float a = (float)num1/num2;
-        float b = a*1000;
-        answer = (int)Math.floor(b);    
-        }
-        return answer;
+      int answer=0;
+      double doubleAnswer=(double)num1 /num2 *1000;  
+      answer=(int)doubleAnswer;
+      return answer;
     }
 }
