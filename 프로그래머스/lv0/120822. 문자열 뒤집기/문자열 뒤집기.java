4d902e020cbcd584f1c52c7cfc5397dd6e8730df
HyonHyon <khm3626@gmail.com>
2023-07-20 23:18:24 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,12 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";      
-        for(int i=0; i<my_string.length();i++){
-             answer += my_string.charAt(my_string.length()-1-i);   
-        }
-        return answer;
+        
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(my_string);
+        sb1.reverse();
+        
+        return sb1.toString();
+        
+        
     }
 }
