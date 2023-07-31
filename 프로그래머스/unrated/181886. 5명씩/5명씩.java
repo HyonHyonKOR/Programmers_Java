f2021ea5948526a3b272d5efd396ec8fa5a0669f
HyonHyon <khm3626@gmail.com>
2023-07-31 17:37:07 +0900

    [unrated] Title: 5명씩, Time: 2.27 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String[] solution(String[] names) {
+       	StringBuilder sb = new StringBuilder();
+    	
+    	for(int i=0; i<names.length; i++) {
+    		if(i%5==0) sb.append(names[i]+","); 
+    	}
+    	
+    	return sb.toString().split(",");
+    	
+    }
+}
