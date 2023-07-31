d865b8e79995200782f5eb0e69770dda980169bd
HyonHyon <khm3626@gmail.com>
2023-07-31 21:35:54 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int size = num_list.length;
+        int[] answer = new int[size];
+        
+        System.arraycopy(num_list,n,answer,0,size-n);
+        System.arraycopy(num_list,0,answer,size-n, n);
+        return answer;
+    }
+}
