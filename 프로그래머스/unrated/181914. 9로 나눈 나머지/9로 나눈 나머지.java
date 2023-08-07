686a43c46b134ab9aed843ae29ad149f7dc14a8e
HyonHyon <khm3626@gmail.com>
2023-08-08 01:04:32 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String number) {
+        
+        int answer = 0;
+        
+        for(int i=0; i< number.length();i++){
+            answer += (number.charAt(i))-48;
+        }
+        
+    
+        return (answer % 9);
+    }
+}
