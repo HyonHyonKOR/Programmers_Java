f11db0a357c5ce49a471ef671cebbb6fc58c882e
HyonHyon <khm3626@gmail.com>
2023-08-07 12:16:06 +0900

    [level 0] Title: 숫자 찾기, Time: 0.25 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,7 +1,19 @@
 class Solution {
     public int solution(int num, int k) {
-        String str = num + "";
-        int answer = str.indexOf(k+"")+1;
-        return (answer> 0)? answer:-1;
+        int answer = 0;
+        int count  = 1;
+        while(num / 10 > 0) {
+            if(num % 10 == k) {
+                answer = count;
+            }
+            num /= 10;
+            ++count;
+        }
+        if(num % 10 == k) {
+            answer = count;
+        }
+        System.out.println(answer);
+        System.out.println(count);
+        return answer > 0 ? count-answer+1 : -1;
     }
 }
