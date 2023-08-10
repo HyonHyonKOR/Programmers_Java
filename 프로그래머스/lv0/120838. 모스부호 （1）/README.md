c5f13468943492d396e352369728d6dc775c6119
HyonHyon <khm3626@gmail.com>
2023-08-11 01:42:32 +0900

    [level 0] Title: 모스부호 (1), Time: 0.19 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/README.md
+++ 프로그래머스/lv0/120838. 모스부호 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 모스부호 (1) - 120838 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120838) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120838?language=java) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 0.19 ms
+메모리: 73 MB, 시간: 0.19 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -2,18 +2,13 @@ import java.util.*;
 
 class Solution {
     public String solution(String letter) {
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         String[] divideLetter = letter.split(" ");
         StringBuilder answer = new StringBuilder();
         
         for(int i =0; i< divideLetter.length; i++){
-            answer.append((char)((Arrays.asList(mos).indexOf(divideLetter[i]))+97));   
+            answer.append((char)((Arrays.asList(morse).indexOf(divideLetter[i]))+97));   
         }
         return answer.toString();
     }
-    
-    String[] mos = {".-","-...","-.-.","-..",".","..-.",
-                    "--.","....","..",".---","-.-",".-..",
-                    "--","-.","---",".--.","--.-",".-.",
-                    "...","-","..-","...-",".--","-..-",
-                    "-.--","--.."};
 }
